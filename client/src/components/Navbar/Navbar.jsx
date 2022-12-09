757cd469e34aa4c1feea98d8b4b9fe2e9b4af2d1
TARA CHAND KUMAWAT <tarachand122333@gmail.com>
2022-12-09 17:26:08 +0530

    User Varification front-end done

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {BiSearch} from 'react-icons/bi'
 import {FaRegUser} from 'react-icons/fa'
 import {BsCart3} from 'react-icons/bs'
 
-const NavbarSM = () =>{
+
+// Login File
+import SignIn from '../Auth/SignIn'
+
+const NavbarSM = ({SignIn,SignUp}) =>{
   return(
     <>
       <div className='flex flex-col gap-1 w-full mx-4'>
@@ -13,7 +17,7 @@ const NavbarSM = () =>{
             <p className='text-gray-500'>Shop No, 5, Rawan Gate Rd, Gokulpura, Jaipur,Rajasthan</p>
           </div>
           <div className='flex justify-center items-center'>
-            <FaRegUser className='text-2xl justify-center items-center cursor-pointer' />
+            <FaRegUser className='text-2xl justify-center items-center cursor-pointer' onClick={SignIn} />
           </div>
         </div>
 
@@ -29,7 +33,7 @@ const NavbarSM = () =>{
   )
 }
 
-const NavbarLG = ()=>{
+const NavbarLG = ({SignIn,SignUp})=>{
   return(
     <>
       <div className='flex justify-between items-center w-full px-8 gap-10 py-2'>
@@ -50,7 +54,7 @@ const NavbarLG = ()=>{
                 </div>
             </div>
             <div className='flex justify-center items-center px-6'>
-                <h1 className='text-gray-600 font-semibold text-xl cursor-pointer'>Login</h1>
+                <h1 className='text-gray-600 font-semibold text-xl cursor-pointer' onClick={SignIn}>Login</h1>
             </div>
         </div>
         <div className='flex gap-2 bg-green py-3 px-2
@@ -64,17 +68,25 @@ const NavbarLG = ()=>{
 }
 
 const Navbar = () => {
+
+  const [openSignin, setOpenSignin] = useState(false);
+  const openSignInmodal = () => setOpenSignin(true);
+
   return (
     <>
+
+      <SignIn isOpen={openSignin} setIsOpen={openSignInmodal} />
+
+
       {/* Navbar for mobile */}
       <div className='flex md:hidden'>
-        <NavbarSM />
+        <NavbarSM SignIn={openSignInmodal} />
       </div>
 
       {/* Navbar for PC */}
       <div className='hidden lg:flex'>
         {/* <h1>Navbar for PC</h1> */}
-        <NavbarLG />
+        <NavbarLG SignIn={openSignInmodal} />
       </div>
     </>
   )
